f515d3b67c94c8baa8bcfc326e22b6ebac5dcaa8
eder lobo <ederandreslobo1998@gmail.com>
2024-07-08 08:42:28 -0500

    updating scripts for contract configuration

--- scripts/add-validator.js
+++ scripts/add-validator.js
@@ -1,16 +1,17 @@
 const ethers = require('ethers');
 const fs = require('fs');
-const { ActionId } = require('./util');
+
 const abi = JSON.parse(fs.readFileSync('./abi/Bridge.abi', 'utf8'));
+const { ActionId } = require('./util');
 require('dotenv').config();
 
 const { 
-    BRIDGE_ADDR, 
-    RPC_PROVIDER, 
-    PRIVATE_KEY, 
-    WRAPPED_TOKEN_ADDR,
-    CHAIN_ID
-  } = process.env
+  BRIDGE_ADDR, 
+  RPC_PROVIDER, 
+  PRIVATE_KEY, 
+  VALIDATOR_ADDR,
+  CHAIN_ID,
+} = process.env
 
 const VALIDATORS_PK = process.env.VALIDATORS_PK.split('|');
 
@@ -19,12 +20,11 @@ const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(BRIDGE_ADDR, abi, signer);
 
 const nowPlus1Hr = Math.floor(new Date().getTime()) + 3600000;
-const fee = ethers.utils.parseUnits('0', 'ether');
 const chainId = Number(CHAIN_ID);
 
-async function hashAndSign(actionSupport, tokenAddress, fee, nonce, contractAddress, expiration, chainId) {
-    const types = ['uint256', 'address', 'uint256', 'uint256', 'address', 'uint256', 'uint32'];
-    const values = [actionSupport, tokenAddress, fee, nonce, contractAddress, expiration, chainId];
+async function hashAndSign(actionSupport, tokenAddress, nonce, contractAddress, expiration, chainId) {
+    const types = ['uint256', 'address', 'uint256', 'address', 'uint256', 'uint32'];
+    const values = [actionSupport, tokenAddress, nonce, contractAddress, expiration, chainId];
 
     const messageHash = ethers.utils.solidityKeccak256(types, values);
 
@@ -41,12 +41,12 @@ async function hashAndSign(actionSupport, tokenAddress, fee, nonce, contractAddr
 async function main() {
     let nonce = Number((await contract.nonce()).toString());
 
-    const signatures = await hashAndSign(ActionId.AddSupportedWrappedToken, WRAPPED_TOKEN_ADDR, fee, nonce, BRIDGE_ADDR, nowPlus1Hr, chainId);
+    const signatures = await hashAndSign(ActionId.AddValidator, VALIDATOR_ADDR, nonce, BRIDGE_ADDR, nowPlus1Hr, chainId);
     console.log('Signatures:', signatures);
-    const tx = await contract.addSupportedWrappedToken(signatures, WRAPPED_TOKEN_ADDR, fee, nowPlus1Hr);
+    const tx = await contract.addValidator(signatures, VALIDATOR_ADDR, nowPlus1Hr);
     await tx.wait();
 
-    console.log('Wrapped token support added');
+    console.log('Validator added');
 }
 
 main().catch((error) => {
